6d3c81b7f935d4d75356071fed3f5511114772b7
yeseorizi <kys50728@g.kmou.ac.kr>
2024-06-07 17:41:49 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,11 +2,11 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3e016bc7ed734d7db07cee5101375aa6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionPageId:3e016bc7ed734d7db07cee5101375aa6
+  rootNotionPageId: null, 
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
